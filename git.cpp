4068bcc94d7d1f7f86beb52c90a0e977ee24cadf
sunshixd <167434443+sunshixd@users.noreply.github.com>
2024-06-10 12:02:14 +0300

    Update git.cpp

--- git.cpp
+++ git.cpp
@@ -6,13 +6,4 @@ int main()
 {
     std::cout << "Hello Daria Vasilevna!\n";
     std::cout << "Vash predmet ochen' interesnii!\n";
-=======
-    std::cout << "Goodbye MDK!\n";
->>>>>>> 63dcc5828aba2165cc218501c1fde69613c25594
-=======
-    std::cout << "Hello World!\n";
->>>>>>> parent of 5e8ba12 (Version 1.1)
-=======
-    std::cout << "Hello Dmitrij!\n";
->>>>>>> parent of 63dcc58 (Version 3.0)
 }
